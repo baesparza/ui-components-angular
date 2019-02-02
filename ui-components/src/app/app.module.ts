f5e4aff631363e7b43e43b269da5b1a281f76bf8
baesparza <bruno.be81@gmail.com>
2019-02-02 15:34:17 -0500

    style: add text theme

--- ui-components/src/app/app.module.ts
+++ ui-components/src/app/app.module.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThemeModule } from './modules/theme/theme.module';
-import { lightTheme, darkTheme } from './modules/theme/data/themes.data';
+import { lightTheme } from './modules/theme/data/themes.data';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     ThemeModule.forRoot({
-      themes: [lightTheme, darkTheme],
-      active: 'dark'
+      themes: [lightTheme],
+      active: 'light'
     })
   ],
   declarations: [
--- ui-components/src/app/modules/theme/data/themes.data.ts
+++ ui-components/src/app/modules/theme/data/themes.data.ts
@@ -1,21 +1,5 @@
 import { Theme } from '../interfaces/theme.interface';
 
-export const darkTheme: Theme = {
-  name: 'dark',
-  properties: {
-    '--background': '#1F2125',
-    '--on-background': '#fff',
-    '--primary': '#2F90FF',
-    '--on-primary': '#fff',
-    '--secondary': '#474A4F',
-    '--on-secondary': '#fff',
-    '--surface': '#282A2F',
-    '--on-surface': '#dddede',
-    '--error': '#E74E3C',
-    '--on-error': '#fff'
-  }
-};
-
 export const lightTheme: Theme = {
   name: 'light',
   properties: {
